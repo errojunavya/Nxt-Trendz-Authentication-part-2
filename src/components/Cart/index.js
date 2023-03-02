6c0b20c26cbaa06b38866be4d48dc61582ba99de
errojunavya <errojunavya@gmail.com>
2023-03-02 05:03:49 +0000

    solution

---
+++ src/components/Cart/index.js
@@ -0,0 +1,18 @@
+// Write your JS code here
+import Header from '../Header'
+
+import './index.css'
+
+const Cart = () => (
+  <div>
+    <Header />
+    <div className="container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png "
+        alt="cart"
+        className="image"
+      />
+    </div>
+  </div>
+)
+export default Cart
